188eabed0ac0055d72eab762cb9f14d5fe4df4c8
zhuqisong <zqsong0330@qq.com>
2021-05-04 12:51:16 +0800

    add list

--- TestModule/src/main/java/Main.java
+++ TestModule/src/main/java/Main.java
@@ -1,6 +1,9 @@
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-        String str = new String();
+        String str = "";
+        ArrayList<Integer> list = new ArrayList<>();
     }
 }
